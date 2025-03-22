26010b4c6c637050e76f6bd924528889fd367155
Mathieu VALENTIN <hmatico@Mathieus-MacBook-Pro.local>
2025-03-22 13:46:24 +0100

    quiet: use mri proc

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,3 @@
 export const fireEvent = (eventName, data) => {
-    if (!eventName?.length) {
-        throw new Error('Event should have name !')
-    }
     document.dispatchEvent(new CustomEvent(eventName, { detail: data }));
 }
