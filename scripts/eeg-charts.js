76f987b15602d0333a7debe333a47cf77495e09b
roehri <nicolas.roehri@unige.ch>
2025-03-22 11:46:57 +0100

    added binding to handle time instant between eeg-charts.js and topo-helper.js

--- scripts/eeg-charts.js
+++ scripts/eeg-charts.js
@@ -1,50 +1,67 @@
-document.addEventListener('getData', (event) => {
-    let instant_t = 125;
-    console.log(event.detail.data)
-    const time_length = event.detail.data[0].length;
-    const eegMatrix = event.detail.data;
-    const time = Array.from({ length: time_length }, (_, i) => i);
-    const channelNames = event.detail.channels;
-    const graphDiv = document.getElementById('eegPannel')
-    const layout = {
-        title: {
-            text: 'EEG'
-        },
-        xaxis: {
-            title: {
-            text: 'Time [s]'
-            },
-            showgrid: false,
-            zeroline: false,
-        },
-        yaxis: {
-            title: {
-            text: 'Channels'
-            },
-            tickvals: channelNames.map((_, i) => i * 10), // Map channel indices to positions
-            ticktext: channelNames, // Display channel names on the y-axis
-            showline: false,
-        },
-    };
-    layout.showlegend = false;
-    let data = eegMatrix.map((channelData, _index) => ({
-        x: time,
-        y: channelData,
-        type: 'scatter',
-        line: { color: 'black' }
-    }));
-    const red_line = [{
-        x: instant_t,
-        y: [0, 100],
-        type: 'scatter',
-        line: { color: 'red' }
-    }];
 
-    data.push({
-        x: [instant_t, instant_t],
-        y: [0, 100],
-        type: 'scatter',
-        line: { color: 'red'} 
-    });
-    Plotly.newPlot(graphDiv, data, layout);
-});
+export function eegChartBinding(topoDisplay){
+	
+	document.addEventListener('getData', (event) => {
+		
+		//console.log(event.detail.data)
+		const time_length = event.detail.data[0].length;
+		const eegMatrix = event.detail.data;
+		const sampRate = event.detail.sampRate;
+		let instant_t = time_length/2;
+		instant_t = instant_t/sampRate;
+		const time = Array.from({ length: time_length }, (_, i) => i/sampRate);
+		const channelNames = event.detail.channels;
+		let graphDiv = document.getElementById('eegPannel')
+		const layout = {
+			title: {
+				text: 'EEG'
+			},
+			xaxis: {
+			  title: {
+				text: 'Time [s]'
+			  },
+			  automargin: true,
+			  tickfont: { size: 14}
+		  },
+		  yaxis: {
+			  title: {
+				text: 'Amplitude'
+			  },
+			  automargin: true,
+			  tickfont: { size: 14}
+		  },
+		  hovermode: 'closest',
+		  showlegend: false,
+		  shapes: [{
+			type: 'line',
+			x0: instant_t,
+			y0: 0,
+			x1: instant_t,
+			yref: 'paper',
+			y1: 1,
+			line: {
+			  color: 'red',
+			  width: 4,
+			  dash: 'dot'
+			}
+		  }]
+		};
+		let data = eegMatrix.map((channelData, _index) => ({
+			x: time,
+			y: channelData,
+			type: 'scatter',
+			line: { color: 'black' }
+		}));
+		
+		Plotly.newPlot(graphDiv, data, layout);
+		graphDiv.on('plotly_click', function(click){
+			console.log('coucou');
+			let currTimeIdx = click.points[0].pointIndex;
+			layout.shapes[0].x0 = time[currTimeIdx];
+			layout.shapes[0].x1 = time[currTimeIdx];
+			Plotly.redraw(graphDiv);
+			let currentTopo = eegMatrix.map(chanData => chanData[currTimeIdx]);
+			topoDisplay.timeUpdate(currentTopo);
+		})
+	});
+}
--- scripts/topo-helper.js
+++ scripts/topo-helper.js
@@ -1,8 +1,6 @@
-import * as niivue from "./niivue.0.46.0.js";
-
 export default class TopoDisplay {
 	
-	constructor(idCanvas) {
+	constructor(idCanvas, niivue) {
 
 		const opts = {
 				show3Dcrosshair: false,
@@ -15,12 +13,17 @@ export default class TopoDisplay {
 		};
 		//this.EEG_LAYOUT_DIR = "./EEG_layout_templates";
 		this.nvTopo = new niivue.Niivue(opts);
+		this.niivue = niivue;
 		this.nvTopo.attachTo(idCanvas);
 	}
   
 	async loadEEGTopo(data) {
 		const EEG_LAYOUT_DIR = "./EEG_layout_templates";
-		let channelList = data.detail.channels;
+		const channelList = data.detail.channels;
+		const eegMatrix = data.detail.data;
+		const rangeMin = Math.min(...eegMatrix.map((chanData,i) => Math.min(...chanData)));
+		const rangeMax = Math.max(...eegMatrix.map((chanData,i) => Math.max(...chanData)));
+		const absRange = Math.max(Math.abs(rangeMin), rangeMax);
 		let response = await fetch(EEG_LAYOUT_DIR + "/" + 'meshes_config.json', {});
 		let meshesJsonConfig = await response.json();
 		const nFiles = meshesJsonConfig.files.length;
@@ -32,8 +35,8 @@ export default class TopoDisplay {
 			let sameChannels = channelList.length === channelLabels.length && channelList.every((value, index) => value === channelLabels[index]);
 			// compare EEG channels from EEG data with EEG channels in JSON
 			if ( sameChannels ) { // if EEG channels are the same in data and JSON 
-				console.log(channelLabels);
-				console.log(currentMeshConfig.file);
+				//console.log(channelLabels);
+				//console.log(currentMeshConfig.file);
 				const urlGifti = './' + EEG_LAYOUT_DIR + '/' + currentMeshConfig.file;
 				let meshLayersList = this.createMeshInfo(urlGifti);
 				await this.nvTopo.loadMeshes([
@@ -44,10 +47,13 @@ export default class TopoDisplay {
 				]);
 				
 				//let currTopo = Array.from({length: channelList.length}, () => Math.random() * 10);
-				let currTopo = data.detail.data.map(chanData => chanData[0]);
+				
+				let currTopo = data.detail.data.map(chanData => chanData[125]);
 				this.nvTopo.meshes[0].layers[0].values.set(currTopo,0);
 				this.nvTopo.setMeshLayerProperty(this.nvTopo.meshes[0].id, 0, "frame4D", 0);
 				this.nvTopo.setMeshShader(this.nvTopo.meshes[0].id, "Diffuse");
+				this.nvTopo.setMeshLayerProperty(this.nvTopo.meshes[0].id, 0, "cal_max", absRange);
+				this.nvTopo.setMeshLayerProperty(this.nvTopo.meshes[0].id, 0, "cal_min", -absRange);
 				this.nvTopo.setRenderAzimuthElevation(0, 90);
 				break
 			}
@@ -65,6 +71,11 @@ export default class TopoDisplay {
 			  opacity: 1,
 		}];
 		return meshInfo
+	}
+	
+	timeUpdate(eegDataT){
+		this.nvTopo.meshes[0].layers[0].values.set(eegDataT,0);
+		this.nvTopo.setMeshLayerProperty(this.nvTopo.meshes[0].id, 0, "frame4D", 0);
 	}
   
 };
