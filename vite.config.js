624c6f851f009d320f0a4acfe13ffce3a7f1e860
Jeroen Buil <185899346+JeroenBuil@users.noreply.github.com>
2026-05-13 16:44:00 +0200

    Add github actions test + deploy (#3)
    * Squashed commit of the following:
    
    commit dcff1519505e71c82b96fb7b3be8bdbd48e8d93c
    Author: Jeroen Buil <185899346+JeroenBuil@users.noreply.github.com>
    Date:   Wed May 13 15:24:33 2026 +0200
    
        Add unit tests + test.yml
    
    * Add deployment action (#2)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -6,10 +6,20 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), svgr()],
+  base: '/videpe/',
+  plugins: [
+    tailwindcss(),
+    react(),
+    svgr(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), //allows the use of a @ alias for imports e.g. @components/ThemeToggle instead of ../components/ThemeToggle
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.js',
   },
 });
