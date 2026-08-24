647351624a5e82a03317ddbb50ddf7e91fff5a21
Jeroen Buil <185899346+JeroenBuil@users.noreply.github.com>
2026-08-24 12:14:29 +0200

    chore: add version number to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       )}
     >
       <p className="text-sm text-foreground">
-        &copy; {new Date().getFullYear()} Clinical NeuroScience Department HUG. All rights reserved.
+        VIDEPE v0.15.0 &copy; {new Date().getFullYear()} Clinical NeuroScience Department HUG. All rights reserved.
       </p>
     </footer>
   );
