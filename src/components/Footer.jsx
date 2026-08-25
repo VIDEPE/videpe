783719e75fcb06ec296f5a52e4c9501d4b2be4a2
Jeroen Buil <185899346+JeroenBuil@users.noreply.github.com>
2026-08-25 17:21:33 +0200

    chore: bump version to 0.16.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       )}
     >
       <p className="text-sm text-foreground">
-        VIDEPE v0.15.0 &copy; {new Date().getFullYear()} Clinical NeuroScience Department HUG. All
+        VIDEPE v0.16.0 &copy; {new Date().getFullYear()} Clinical NeuroScience Department HUG. All
         rights reserved.
       </p>
     </footer>
